b27236c194ec3d4407c5a079e43dbe85c3bc84f6
Hidde Nijland <hnijland@email.uscupstate.edu>
2017-04-16 21:55:25 +0000

    Add account activation... 1 error for pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,6 @@ User.create!(name:  "Hidde Nijland",
              email: "hidde.nijland@googlemail.com",
              password:              "Inspiron4711",
              password_confirmation: "Inspiron4711",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
