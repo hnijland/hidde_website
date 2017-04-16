c634320c709c76cb30050b767b26555309d60415
Hidde Nijland <hnijland@email.uscupstate.edu>
2017-04-16 20:14:35 +0000

    Deleted sample users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,5 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Hidde Nijland",
+             email: "hidde.nijland@googlemail.com",
+             password:              "Inspiron4711",
+             password_confirmation: "Inspiron4711",
              admin: true)
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
